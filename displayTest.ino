3766452e28c0bffb4f8b5fea31adfe24cf4e35eb
thediamonder11 <76886710+thediamonder11@users.noreply.github.com>
2024-12-30 17:34:53 +0000

    Update displayTest.ino

--- displayTest.ino
+++ displayTest.ino
@@ -22,7 +22,7 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(2, OUTPUT);
-   pinMode(3, OUTPUT);
+  pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
